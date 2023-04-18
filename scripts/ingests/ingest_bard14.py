100028f60d09d5a15b3e7fa389ccb612c44c7fe7
Jolie L'Heureux <jel2227@columbia.edu>
2023-04-17 22:15:01 -0400

    script to ingest new spectra links for bard14 data

--- scripts/ingests/ingest_bard14.py
+++ scripts/ingests/ingest_bard14.py
@@ -2,7 +2,7 @@ from scripts.ingests.ingest_utils import *
 from scripts.ingests.utils import *
 
 
-SAVE_DB = False  # save the data files in addition to modifying the .db file
+SAVE_DB = False # save the data files in addition to modifying the .db file
 RECREATE_DB = False  # recreates the .db file from the data files
 
 logger.setLevel(logging.DEBUG)
@@ -23,14 +23,9 @@ for index, row in data.iterrows():
     spectrum_url = row['Spectrum'] #goes to specific column entry finds the url
     object_name = row['Source']
     with db.engine.begin() as conn:
-        conn.execute(db.Spectra.update()\
-                .where(db.Spectra.c.source == object_name)\
-                .values(spectrum = spectrum_url ))
-        conn.execute(db.Spectra.update()\
-                .where(db.Spectra.c.source == object_name)\
-                .values(original_spectrum = original_spectrum))
-
+        conn.execute(db.Spectra.update().where(db.Spectra.c.source == object_name).values(spectrum = spectrum_url ))
+        conn.execute(db.Spectra.update().where(db.Spinectra.c.source == object_name).values(original_spectrum = original_spectrum))
 
     # WRITE THE JSON FILES
-   # if SAVE_DB:
-       # db.save_database(directory='data/')"""
+    if SAVE_DB:
+        db.save_database(directory='data/')
